ddc0726d86333f13ab6e43debcca4508ee8f603e
Schoyen <oyvindschoyen@gmail.com>
2014-09-02 16:00:21 +0200

    second

--- Oblig1/Project1.cpp
+++ Oblig1/Project1.cpp
@@ -3,3 +3,32 @@
 
 using namespace std;
 using namespace arma;
+
+void TDMA(int n) 
+{
+
+    /*
+     * Consider using int instead
+     */
+    vec a = zeros<vec>(n);
+    vec b = zeros<vec>(n);
+    vec c = zeros<vec>(n);
+    vec gamma = zeros<vec>(n);
+    vec lambda = zeros<vec>(n);
+    vec b_func = zeros<vec>(n);
+    vec y = zeros<vec>(n); 
+    double h = 1.0/(n+1);
+    double x = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        a[i] = -1;
+        b[i] = 2;
+        c[i] = -1;
+        b_func[i] = h*h*100*exp(-10*x);
+        x = i*h;
+
+    }
+
+}
