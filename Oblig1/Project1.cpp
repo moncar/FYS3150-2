6f1713087bfbb3331921d0ace4458e8c88b8cf8d
Schoyen <oyvindschoyen@gmail.com>
2014-09-04 15:55:08 +0200

    04.09-14 first commit

--- Oblig1/Project1.cpp
+++ Oblig1/Project1.cpp
@@ -1,33 +1,32 @@
 # include <cmath>
 # include <string>
 # include <armadillo>
+# include <chrono>
 
 using namespace std;
 using namespace arma;
+using namespace chrono;
 
-vec TDMA(int n, vec a, vec b, vec c) 
+vec TDMA(int n, vec a, vec b, vec c, vec b_func) 
 {
 
     vec gamma = zeros<vec>(n);
     vec lambda = zeros<vec>(n);
-    vec b_func = zeros<vec>(n);
     vec v = zeros<vec>(n);
-    double h = 1.0/(n+1);
-    double x = 0;
+
+    // Starting clock
+    auto start = high_resolution_clock::now();
     
     /*
      * Implementing the Thomas Algorithm
      */
 
     // Forward substitution
-    b_func[0] = h*h*100*exp(-10*x);
     lambda[0] = b[0];
     v[0] = b_func[0]/lambda[0];
     for (int i = 1; i < n; i++)
     {
 
-        x = i*h;
-        b_func[i] = h*h*100*exp(-10*x);
         gamma[i] = c[i-1]/lambda[i-1];
         lambda[i] = b[i] - gamma[i]*a[i];
         v[i] = (b_func[i] - a[i]*v[i-1])/lambda[i];
@@ -43,76 +42,50 @@ vec TDMA(int n, vec a, vec b, vec c)
 
     }
 
+    auto finish = high_resolution_clock::now();
+    cout << "Duration of TDMA: " << duration_cast<nanoseconds>(finish - start).count()
+         << "ns" << endl;
+
+
     return v;
 
 };
 
 
-
-vec GE(int n, vec a, vec b, vec c)
+vec Gaussian(int n, vec a, vec b, vec c, vec b_func)
 {
 
-    vec v = zeros<vec>(n);
     mat A = zeros(n, n);
-    double h = 1.0/(n+1);
-    double x = 0;
+    vec v;
 
     A(0, 0) = b[0];
-    A(0, 1) = b[1];
-
-
-
-};
-
-vec LU(int n, vec a, vec b, vec c)
-{
-
-    vec v = zeros<vec>(n);
-    vec gamma = zeros<vec>(n);
-    vec lambda = zeros<vec>(n);
-    vec y = zeros<vec>(n);
-    mat A = zeros(n, n);
-    mat L, U, P;
-    double h = 1.0/(n+1);
-    double x = 0;
-
-    A(0, 0) = b[0];
-    A(0, 1) = b[1];
+    A(0, 1) = c[1];
 
     for (int i = 1; i < n-1; i++) {
-        A(i-1, i) = a[i];
+        A(i, i-1) = a[i];
         A(i, i) = b[i];
-        A(i+1, i) = c[i];
+        A(i, i+1) = c[i];
     }
 
     A(n-1, n-2) = a[n-1];
     A(n-1, n-1) = b[n-1];
 
-    lu(L, U, P, A);
+    auto start = high_resolution_clock::now();
 
-    y[0] = h*h*exp(-10*x);
-    gamma[0] = y[0]*L(1, 0);
-    for (int i = 1; i < n; i++) {
+    v = solve(A, b_func);
 
-        x = i*h;
-        y[i] = h*h*exp(-10*x) - gamma[i-1];
-        for (int j = 0; j < i; j++) {
-
-            gamma[i] += L(i, j)*y[j];
-
-        }
-    }
-
-    lambda[n-1] = U(n-1, n-1);
-    for (int i = n-1, i >= 0; i--) {
-
-        v[i] = y[i]/lambda[i];
-
-        for (int j = n-1; j >= i; 
-
-    }
+    auto finish = high_resolution_clock::now();
+    cout << "Duration of Gaussian Elimination (without the construction of the matrix A): "
+         << duration_cast<nanoseconds>(finish - start).count()
+         << "ns" << endl;
 
     return v;
+
+};
+
+
+void Plot(vec x, vec v)
+{
 
 };
 
@@ -130,10 +103,13 @@ int main(int argc, char* argv[])
     }
 
     int n = atoi(argv[1]);
+    vec x = zeros<vec>(n);
+    double h = 1.0/(n+1);
 
     vec a = zeros<vec>(n);
     vec b = zeros<vec>(n);
     vec c = zeros<vec>(n);
+    vec b_func = zeros<vec>(n);
     vec v;
 
     a.fill(-1);
@@ -142,10 +118,16 @@ int main(int argc, char* argv[])
     a[0] = 0;
     c[n-1] = 0;
 
-    v = LU(n, a, b, c);
-    v = TDMA(n, a, b, c);
+    b_func[0] = h*h*100*exp(-10*x[0]);
+    for (int i = 1; i < n; i++) {
 
+        x[i] = i*h;
+        b_func[i] = h*h*100*exp(-10*x);
 
+    }
+
+    v = Gaussian(n, a, b, c, b_func);
+    v = TDMA(n, a, b, c, b_func);
 
     return 0;
 
