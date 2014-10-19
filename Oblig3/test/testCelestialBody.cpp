4b7738c6c12487b4e140e46d671029dc4ef0c13e
Schoyen <oyvindschoyen@gmail.com>
2014-10-19 20:42:25 +0200

    Fix undefined reference

--- Oblig3/test/testCelestialBody.cpp
+++ Oblig3/test/testCelestialBody.cpp
@@ -4,10 +4,19 @@
 # include "../src/CelestialBody.h"
 # include "../src/vec3.h"
 
-TEST(BasicTest, Handles) {
-    ASSERT_TRUE(true);
+TEST(ParameterInitialization, Celest) {
+    std::string name = "TestPlanet";
+    double mass = 1.0e+24;
+    vec3 r (0.0, 0.0, 0.0);
+    vec3 v (0.0, 0.0, 0.0);
+    CelestialBody cb (name, mass, &r, &v);
+    ASSERT_EQ(name, cb.name);
+    ASSERT_EQ(mass, cb.mass);
+    ASSERT_EQ(r.x(), (cb.r)->x());
+    ASSERT_EQ(v.x(), (cb.v)->x());
 }
 
-int main() {
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
